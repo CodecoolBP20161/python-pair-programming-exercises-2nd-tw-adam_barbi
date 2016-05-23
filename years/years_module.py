6c1d4c0f9098cc564168adfaafe86697f780a3b6
adambdnr <adambodnar@adams-air.t.hu>
2016-05-23 14:36:18 +0200

    megcsináltuk a funkciót

--- years/years_module.py
+++ years/years_module.py
@@ -1,13 +1,25 @@
-import datetime
+from datetime import datetime
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+#hundred = 100 - age
+#current_year = datetime.now().year
+#tomb = current_year + hundred
+
+#print("You'll be 100 years old in %d" % tomb)
+tomb = ()
 
 
 def years(age):
-    return
+    hundred = 100 - age
+    current_year = datetime.now().year
+    tomb = current_year + hundred
+    return tomb
 
 
 def main():
+    print("You'll be 100 years old in %d" % years(age))
     return
-
 
 if __name__ == '__main__':
     main()
