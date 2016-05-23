027787d0781d8c0dcdafe967d47f7912e295984b
chibbol <barbara.cegledi@gmail.com>
2016-05-23 15:08:25 +0200

    f.b. push test

--- fizzbuzz/fizzbuzz_module.py
+++ fizzbuzz/fizzbuzz_module.py
@@ -1,8 +1,19 @@
 def fizzbuzz(number):
-    return
+    value = ""
+    if number % 15 == 0:
+        value = ("FizzBuzz")
+    elif number % 5 == 0:
+        value = ("Buzz")
+    elif number % 3 == 0:
+       value = ("Fizz")
+    else:
+       value = number
+    return value
 
 
 def main():
+    for i in range(1, 101):
+        print(fizzbuzz(i))
     return
 
 if __name__ == '__main__':
