f2b06fa9597257b3e3be211e7ed72be1869fc901
adambdnr <adambodnar@Adams-MacBook-Air.local>
2016-05-27 16:18:46 +0200

    palindrome is ready

--- palindrome/palindrome_module.py
+++ palindrome/palindrome_module.py
@@ -1,9 +1,16 @@
 def palindrome(str):
-    return
+    string = input('Please enter a string to check: ')
+    without_space = string.replace(' ','')
+    reverse = without_space[::-1]
+    if without_space == reverse:
+        return("That's a palindrome!")
+    else:
+        return("That's not a palindrome!")
 
 
 def main():
-    return
+    print(palindrome(str))
+
 
 
 if __name__ == '__main__':
