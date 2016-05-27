0c2a342b566c169ca4ff66527bef094d352c41bc
adambdnr <adambodnar@Adams-MacBook-Air.local>
2016-05-27 16:16:18 +0200

    listoverlap is ready

--- listoverlap/listoverlap_module.py
+++ listoverlap/listoverlap_module.py
@@ -1,10 +1,19 @@
 def listoverlap(list1, list2):
-    return
+    for i in list1:
+        if i in my_list:
+            continue
+        if i in list2:
+            my_list.append(i)
+    return (my_list)
 
 
 def main():
+    print(listoverlap(a, b))
     return
 
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+my_list = []
 
 if __name__ == '__main__':
     main()
