db2d8b05777e3648f5af4d1303181bd1d71e7d5f
adambdnr <adambodnar@Adams-MacBook-Air.local>
2016-05-27 16:19:33 +0200

    years is ready

--- years/years_module.py
+++ years/years_module.py
@@ -1,24 +1,17 @@
 from datetime import datetime
-name = input("Enter your name: ")
-age = int(input("Enter your age: "))
-
-#hundred = 100 - age
-#current_year = datetime.now().year
-#tomb = current_year + hundred
-
-#print("You'll be 100 years old in %d" % tomb)
-tomb = ()
 
 
-def years(age):
+def years(age, name):
     hundred = 100 - age
     current_year = datetime.now().year
-    tomb = current_year + hundred
-    return tomb
+    final = hundred + current_year
+    return ('%s, you will be 100 years old in %d' % (name, final))
 
 
 def main():
-    print("You'll be 100 years old in %d" % years(age))
+    name = input("Enter your name: ")
+    age = int(input("Enter your age: "))
+    print(years(age, name))
     return
 
 if __name__ == '__main__':
